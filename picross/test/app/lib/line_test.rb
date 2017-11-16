563c64b24c537ddc3b26898829a96dc16bd67854
David Brady <dbrady@shinybit.com>
2017-11-16 11:05:07 -0700

    Explicitly test delegation in Line#to_s

--- picross/test/app/lib/line_test.rb
+++ picross/test/app/lib/line_test.rb
@@ -10,7 +10,11 @@ class LineTest < MiniTest::Test
     assert_equal 2, subject.size
   end
 
-  def test_to_s
-    assert_equal '??', subject.to_s
+  def test_to_s_delegates_to_cells_to_s
+    cell1 = MiniTest::Mock.new
+    cell2 = MiniTest::Mock.new
+    cell1.expect :to_s, '.'
+    cell2.expect :to_s, '#'
+    assert_equal '.#', Line.new([cell1, cell2]).to_s
   end
 end
