4644d64ce02a07b23594c566487de4d2758442cb
David Brady <dbrady@shinybit.com>
2018-12-12 13:46:52 -0500

    Move game config logic into Poolrad

--- poolrad/poolrad.rb
+++ poolrad/poolrad.rb
@@ -23,25 +23,49 @@ class Poolrad
 
   # Given a save id, like "B1" or "C6", return the filename
   # of that save
-  def save_file(id, ext="SAV")
+  def save_filename(id, ext="SAV")
     "CHRDAT#{id}.#{ext}"
   end
 
-  def backup_save_file(id, ext="SAV")
-    src = File.join(game_path, save_file(id, ext))
-    dst = File.join(game_path, save_file(id, ext + ".BAK"))
+  def save_filepath(id, ext="SAV")
+    File.join game_path, save_filename(id, ext)
+  end
+
+  def backup_save_filename(id, ext="SAV")
+    src = File.join(game_path, save_filename(id, ext))
+    dst = File.join(game_path, save_filename(id, ext + ".BAK"))
     File.cp src, dst
   end
 
+  def backup_save_filepath(id, ext="SAV")
+    File.join game_path, backup_save_filename(id, ext)
+  end
+
+
   def assert_game_configuration!
     if !File.directory?(game_path)
-      raise <<OHNOES
+      barf_to_death! <<OHNOES
 Could not find Pool of Radiance.
 1. Is your game_cfg.yml configured?
 2. Is Pool of Radiance actually installed where game_cfg.yml's "game_data_path"
    says it is?
 OHNOES
     end
+  end
+
+  def assert_save_game_exists!(id, ext="SAV")
+    filepath = save_filepath(id, ext)
+    if !File.exists?(filepath)
+      barf_to_death! <<LOLBARF
+Could not find Pool of Radiance savegame data file at '#{filepath}',
+check your install directory or game_cfg.yml"
+LOLBARF
+    end
+  end
+
+  def barf_to_death!(msg, code=1)
+    puts msg
+    exit code
   end
 end
 
