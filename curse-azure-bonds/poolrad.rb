dc3895299b88ad682af7dc697453d60bc09a3c38
David Brady <dbrady@shinybit.com>
2018-12-18 17:39:04 -0500

    Add Curse of the Azure Bonds
    * DANGER - The scripts were lifted straight over from Pool of Radiance
      and the save file formats DO NOT MATCH. I have been fixing the
      scripts as I need them but for now, try the `get` version of a
      script before you trust the `set` version of it
    
    * get_hp/set_hp had to be adjusted to find the new correct max_hp
    
    * max_hp adjusted to allow CON of 19 or 20, because one of the first
      magic items you get in the game is a Girdle of the Dwarves (+1 CON)
      and I put it on an actual dwarf (which isn't relevant except that he
      had a natural 19 CON)
    
    * Literally haven't even renamed poolrad.rb because I just now
      realized this, lol. Might rename it if I actually figure out more of
      the save file format and want to make a proxy/orm class for Curse

---
+++ curse-azure-bonds/poolrad.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+# poolrad.rb - lib for common Pool of Radiance stuff
+# run as a script to check that the game_cfg.yml is et up right
+require 'yaml'
+require 'fileutils'
+
+class Poolrad
+  def initialize
+  end
+
+  def here
+    File.dirname(__FILE__)
+  end
+  def tool_path; here; end
+
+  def config
+    YAML::load_file(File.join(here, "game_cfg.yml"))
+  end
+
+  def game_path
+    config["game_data_path"]
+  end
+
+  # Given a save id, like "B1" or "C6", return the filename
+  # of that save
+  def save_filename(id, ext="SAV")
+    "CHRDAT#{id}.#{ext}"
+  end
+
+  def save_filepath(id, ext="SAV")
+    File.join game_path, save_filename(id, ext)
+  end
+
+  def backup_savegame!(id, ext="SAV")
+    src = File.join(game_path, save_filename(id, ext))
+    dst = File.join(game_path, save_filename(id, ext + ".BAK"))
+    puts "Backing up #{src} -> #{dst}..."
+    FileUtils.cp src, dst
+  end
+
+  def backup_save_filepath(id, ext="SAV")
+    File.join game_path, backup_save_filename(id, ext)
+  end
+
+  def assert_game_configuration!
+    if !File.directory?(game_path)
+      barf_to_death! <<OHNOES
+Could not find Pool of Radiance.
+1. Is your game_cfg.yml configured?
+2. Is Pool of Radiance actually installed where game_cfg.yml's "game_data_path"
+   says it is?
+OHNOES
+    end
+  end
+
+  def assert_save_game_exists!(id, ext="SAV")
+    filepath = save_filepath(id, ext)
+    if !File.exists?(filepath)
+      barf_to_death! <<LOLBARF
+Could not find Pool of Radiance savegame data file at '#{filepath}',
+check your install directory or game_cfg.yml"
+LOLBARF
+    end
+  end
+
+  def barf_to_death!(msg, code=1)
+    puts msg
+    exit code
+  end
+end
+
+if __FILE__==$0
+  poolrad = Poolrad.new
+  puts "Pool of Radiance is configured at #{poolrad.game_path}"
+  puts "Checking for Pool of Radiance folder..."
+  poolrad.assert_game_configuration!
+  puts "Everything checks out!"
+end
